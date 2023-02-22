156a7992c999ab6819afa7c177714528b0e8fbb9
JoaoAbreu92 <heriquepib@gmail.com>
2023-02-22 20:16:55 -0300

    testando js

---
+++ js/efeitos.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    $("#input-search").on("focus", function () {
+
+        $("li.search").addClass("ativo");
+
+    }).on("blur", function () {
+
+        $("li.search").removeClass("ativo");
+    });
+
+    $(".bnews").owlCarousel({
+        loop: true,
+        margin: 2,
+        nav: true,
+        navText: ["Anterior","Próximo"],
+        responsive: {
+            0: {
+                item: 1
+            },
+            720: {
+                item: 3
+            },
+            1000: {
+                item: 4
+            },
+            
+        }
+    })
+});
